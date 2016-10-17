04112850dc368a162d5f67df39167123d41a3219
“John <johnjamesmartin@live.com>
2016-10-17 22:23:50 +0100

    Tutorial 5 - Implementing POST methods

--- Tut3/app.js
+++ Tut3/app.js
@@ -10,6 +10,11 @@ var port = 8080;
 
 mongoose.connect(db);
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.get('/', function(req, res) {
     res.send('Happy to be here');
 });
@@ -25,6 +30,49 @@ app.get('/books', function(req, res) {
                 res.json(books);
             }
         });
+});
+
+app.get('/books/:id', function(req, res) {
+    console.log('Retrieving the specified book');
+    Book.findOne({
+        _id: req.params.id
+    })
+        .exec(function(err, book) {
+            if (err) {
+                res.send('Error has occurred: ' + err);
+            } else {
+                console.log('Successfully retrieved the specified book')
+                res.json(book);
+            }
+        });
+});
+
+app.post('/book', function(req, res) {
+    var newBook = new Book();
+
+    newBook.title = req.body.title;
+    newBook.author = req.body.author;
+    newBook.category = req.body.category;
+
+    newBook.save(function(err, book) {
+        if (err) {
+            res.send('Error saving book: ' + err);
+        } else {
+            console.log('Successfully saved book: ' + book);
+            res.send(book);
+        }
+    });
+});
+
+app.post('/book2', function(req, res) {
+    Book.create(req.body, function(err, book) {
+        if (err) {
+            res.send('Error creating book: ' + err);
+        } else {
+            console.log('Successfully created book: ' + book);
+            res.send(book);
+        }
+    });
 });
 
 app.listen(port, function() {
